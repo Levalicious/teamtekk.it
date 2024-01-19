2caacb030c7da69c884f339b0cd209eaa49a5219
Levalicious <levknoblock@gmail.com>
2024-01-19 09:50:14 -0500

    [LevK]: Reee222

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type {Config} from 'jest';
 const config: Config = {
   verbose: true,
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.jsx', '.ts', '.tsx']
 };
 
 export default config;
